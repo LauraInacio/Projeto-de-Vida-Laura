31908d903e7b90990f53b4ddea1d3d8f92e1ac35
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2025-06-17 07:55:04 -0300

    r4545

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i=0; i<botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date ("2026-01-15T)00:00:00");
+const tempoObjetivo1 = new Date ("2026-01-15T00:00:00");
 let tempoAtual = new Date();
 
 
