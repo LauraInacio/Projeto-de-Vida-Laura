d00bca92448d4cf77a68414fbfad1551394802f9
LauraInacio <145573341+LauraInacio@users.noreply.github.com>
2025-07-08 08:11:06 -0300

    777

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-07-11T00:00:00")
-const tempoObjetivo2 = new Date("2026-01-17T00:00:00")
-const tempoObjetivo3 = new Date("2026-02-01T00:00:00")
-const tempoObjetivo4 = new Date("2026-01-16T00:00:00")
+const tempoObjetivo1 = new Date("2026-01-15T00:00:00");
+const tempoObjetivo2 = new Date("2031-01-01T00:00:00");
+const tempoObjetivo3 = new Date("2026-01-16T00:00:00");
+const tempoObjetivo4 = new Date("2025-12-17T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
